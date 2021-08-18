8c162b83dac85112e0a79361364ccf6630c94a30
Vitalii <vittiger09@gmail.com>
2021-08-18 13:42:47 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const element = <h1>Hello World! Меня зовут Виталий Компаниец.</h1>;
+
+ReactDom.render(element, document.getElementById("root"));
